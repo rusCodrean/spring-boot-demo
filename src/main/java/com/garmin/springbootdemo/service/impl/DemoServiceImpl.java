d0fdf308cc97c7b833bc96c846d6cae4a35def22
GARMIN\ruso <codrean.rus@garmin.com>
2022-09-21 17:14:45 +0300

    Changed the port and reordered TODOS

--- src/main/java/com/garmin/springbootdemo/service/impl/DemoServiceImpl.java
+++ src/main/java/com/garmin/springbootdemo/service/impl/DemoServiceImpl.java
@@ -51,11 +51,11 @@ public class DemoServiceImpl implements DemoService {
         return dreamRunDto;
     }
 
-    // TODO write a method that removes all duplicates from a list of Integers. Write a Unit Test to demonstrate it works as expected.
-
     // TODO write a method that checks if a given number is Palindrome. Write a Unit Test to demonstrate it works as expected.
+
+    // TODO Create a method that returns a property defined in the application.property file.
 
     // TODO write a method that finds first non-repeated character in a String. Write a Unit Test to demonstrate it works as expected.
 
-    // TODO Create a method that returns a property defined in the application.property file.
+    // TODO write a method that removes all duplicates from a list of Integers. Write a Unit Test to demonstrate it works as expected.
 }
